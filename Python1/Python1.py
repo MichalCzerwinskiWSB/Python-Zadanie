4b42980d84a92d847db1bb07148751f2594896c1
MichalCzerwinskiWSB <michal.czerwinski@student.wsb.edu.pl>
2025-04-22 16:24:00 +0200

    Kalkulator 2 liczb

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,1 +1,20 @@
 
+liczba1 = float(input("Podaj pierwsza liczbe: "))
+liczba2 = float(input("Podaj druga liczbe: "))
+dzialanie = input("Wybierz dzialanie (+, -, * lub /): ")
+
+if dzialanie == "+":
+    wynik = liczba1 + liczba2
+elif dzialanie == "-":
+    wynik = liczba1 - liczba2
+elif dzialanie == "*":
+    wynik = liczba1 * liczba2
+elif dzialanie == "/":
+    if liczba2 != 0:
+        wynik = liczba1 / liczba2
+    else:
+        wynik = "Blad: Dzielenie przez zero!"
+else:
+    wynik = "Nieznana operacja!"
+
+print("Wynik:", wynik)
