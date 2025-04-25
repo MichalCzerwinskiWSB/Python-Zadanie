c694a7fd8726ac35d7fbc7420b318a4cb6a50c3e
MichalCzerwinskiWSB <michal.czerwinski@student.wsb.edu.pl>
2025-04-25 16:53:40 +0200

    Dodanie 3 zadania oraz zmodyfikowanie warunku if

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,7 +1,8 @@
 print("Wybierz program:")
 print("1 - Kalkulator")
 print("2 - Konwerter temperatur")
-wybor = input("Twoj wybor (1/2): ")
+print("3 - Srednia ocen ucznia")
+wybor = input("Twoj wybor (1/2/3): ")
 
 if wybor == "1":
     print("\n--- Kalkulator ---")
@@ -42,5 +43,31 @@ elif wybor == "2":
     else:
         print("Nieprawidlowa operacja.")
 
+elif wybor == "3":
+    print("\n--- Srednia ocen ucznia ---")
+    ilosc_ocen = int(input("Ile ocen chcesz wprowadzic? "))
+    suma = 0
+
+    for i in range(ilosc_ocen):
+        ocena = float(input(f"Podaj ocene {i+1}: "))
+
+        if ocena <= 6:
+            suma += ocena
+        else:
+            print ("Blad: Ocena jest za wysoka. \n Podaj prawidlowa ocene: ")
+            ocena = float(input(f"Podaj ocene {i+1}: "))
+            suma += ocena
+
+        
+        
+
+    srednia = suma / ilosc_ocen
+    print(f"Srednia: {srednia:.2f}")
+
+    if srednia >= 3.0:
+        print("Uczen zdal.")
+    else:
+        print("Uczen nie zdal.")
+
 else:
-    print("Nieznana opcja. Uruchom program ponownie.")
+    print("Nieprawidlowa operacja.")
